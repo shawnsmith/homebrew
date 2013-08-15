54b14038676dededadac84c3db70111995f95d66
Shawn Smith <shawn@bazaarvoice.com>
2013-08-15 09:29:43 -0500

    Update to jsonpps-1.1 with --in-place support.

--- Library/Formula/jsonpps.rb
+++ Library/Formula/jsonpps.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Jsonpps < Formula
   homepage 'https://github.com/bazaarvoice/jsonpps'
-  url 'http://repo1.maven.org/maven2/com/bazaarvoice/jsonpps/jsonpps/1.0/jsonpps-1.0.jar'
-  sha1 'eff26151ffe7b45202aa5343a127b8334d1c2dee'
+  url 'http://repo1.maven.org/maven2/com/bazaarvoice/jsonpps/jsonpps/1.1/jsonpps-1.1.jar'
+  sha1 '6d8abeeb5bb0e70965ac24d7fb77fd2a73b01707'
 
   def install
-    libexec.install "jsonpps-1.0.jar"
-    bin.write_jar_script libexec/"jsonpps-1.0.jar", "jsonpps", "-Xms64m -Xmx64m"
+    libexec.install "jsonpps-1.1.jar"
+    bin.write_jar_script libexec/"jsonpps-1.1.jar", "jsonpps", "-Xms64m -Xmx64m"
   end
 end
